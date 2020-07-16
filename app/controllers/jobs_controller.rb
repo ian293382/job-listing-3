d3ebd2fd423668e280fdc47f17797c041bce2a33
Ian293382 <moshou5571228@gmail.com>
2020-07-16 11:57:29 +0800

    job cintroller reduce

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,6 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy, :update, :create]
-
+  layout 'job'
   def index
     @jobs = case params[:order]
     when 'by_lower_bound'
@@ -13,9 +13,7 @@ class JobsController < ApplicationController
 
   end
 
-  def new
-    @job = Job.new
-  end
+
 
   def show
     @job = Job.find(params[:id])
@@ -26,34 +24,7 @@ class JobsController < ApplicationController
     end
   end
 
-  def edit
-    @job = Job.find(params[:id])
-  end
 
-  def create
-    @job = Job.new(job_params)
-
-    if @job.save
-      redirect_to jobs_path
-    else
-      render :new
-    end
-  end
-
-  def update
-    @job = Job.find(params[:id])
-    if @job.update(job_params)
-      redirect_to jobs_path
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @job = Job.find(params[:id])
-    @job.destroy
-    redirect_to jobs_path , alert: 'Are yoy sure?'
-  end
 
 
 
